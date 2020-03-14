c2be03505f2adc2121bd4a17d3b718b7cffed8cc
tikir <tikir@192.168.1.23>
2020-03-15 00:45:39 +0300

    second problem resolved

--- src/com/problems/problem1342/Problem1342.java
+++ src/com/problems/problem1342/Problem1342.java
@@ -1,6 +1,6 @@
-/*
-1342. Number of Steps to Reduce a Number to Zero
-Description: Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+/*1342. Number of Steps to Reduce a Number to Zero
+
+Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
 
 Example 1:
 
@@ -40,7 +40,7 @@ public class Problem1342 {
 		
         var result = instance.numberOfSteps(input);
         
-        System.out.println(result);
+        //System.out.println(result);
 	}
 	
 	public int numberOfSteps (int num) {
---
+++ src/com/problems/problem1379/TreeNode.java
@@ -0,0 +1,8 @@
+package com.problems.problem1379;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
