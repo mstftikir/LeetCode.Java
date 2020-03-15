c8fc6d83b88b3c710f0aef7f021c23631569ba55
tikir <tikir@192.168.1.23>
2020-03-15 14:14:13 +0300

    Problem1342 resolved

--- src/com/problems/problem1342/Problem1342.java
+++ src/com/problems/problem1342/Problem1342.java
@@ -45,6 +45,7 @@ public class Problem1342 {
 	
 	public int numberOfSteps (int num) {
         int counter = 0;
+        
         while(num > 0){
             if(num % 2 == 0){
                 num /= 2;
