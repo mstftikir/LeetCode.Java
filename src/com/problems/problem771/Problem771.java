9e54c8aa52f04cb26b79f83d8ef8d76e7c426ed1
tikir <tikir@192.168.1.23>
2020-03-15 14:29:47 +0300

    Problem771 solved

---
+++ src/com/problems/problem771/Problem771.java
@@ -0,0 +1,52 @@
+/*771. Jewels and Stones
+
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+ * */
+
+package com.problems.problem771;
+
+public class Problem771 {
+
+	public static void main(String[] args) {
+		
+		var instance = new Problem771();
+		
+		String J = "aA", S = "aAAbbbb";
+		
+		var result = instance.numJewelsInStones(J, S);
+		
+		System.out.println(result);
+	}
+	
+    public int numJewelsInStones(String J, String S) {
+        int counter = 0;
+    	
+        for (int i = 0; i < J.length(); i++) {
+			for (int k = 0; k < S.length(); k++) {
+				if(J.charAt(i) == S.charAt(k)) {
+					counter++;
+				}
+			}
+		}
+        
+    	return counter;
+    	
+    }
+	
+
+}
