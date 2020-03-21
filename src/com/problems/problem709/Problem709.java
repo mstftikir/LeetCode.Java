e86e9de81dfc445fc96d98a623a97a2319fbf2e1
tikir <tikir@192.168.1.23>
2020-03-21 13:00:43 +0300

    Problem709 solved with 2 different approach

---
+++ src/com/problems/problem709/Problem709.java
@@ -0,0 +1,50 @@
+/*709. To Lower Case
+
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+ 
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+ * */
+
+package com.problems.problem709;
+
+public class Problem709 {
+
+	public static void main(String[] args) {
+		var instance = new Problem709();
+		
+		var input = "Hello";
+		
+		var result = instance.toLowerCase(input);
+		
+		System.out.println(result);
+	}
+	
+    public String toLowerCase(String str) {
+        //return str.toLowerCase();
+
+        char[] s = str.toCharArray();
+    	
+    	for(int i = 0; i < s.length; i++) {
+            if(s[i] >= 65 && s[i] <= 90){
+            	char c = (char) (s[i] + 32);
+            	s[i] = c;
+            }
+    	}
+    	
+    	return new String(s);
+    }
+
+}
