7bc260c036b47e93932a3d7f79587ad93aac53f9
Mustafa Tıkır <tikirmustafa@gmail.com>
2020-04-04 14:15:04 +0300

    Problem977 solved with LinkedList ADS

---
+++ src/com/problems/problem977/Problem977.java
@@ -0,0 +1,34 @@
+package com.problems.problem977;
+
+import java.util.*;
+
+public class Problem977 {
+
+    public static void main(String[] args) {
+        var instance =new Problem977();
+
+        int [] input = {-4,-1,0,3,10};
+
+        int [] result = instance.sortedSquares(input);
+
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+
+    public int[] sortedSquares(int[] A) {
+        List<Integer> list = new LinkedList<>();
+
+        for(int i : A){
+            list.add(i * i);
+        }
+
+        Collections.sort(list);
+
+        for (int i = 0; i < A.length ; i++) {
+            A[i] = list.get(i);
+        }
+
+        return A;
+    }
+}
